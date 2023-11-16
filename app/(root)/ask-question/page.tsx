2ab83b7b41c9df9ea5b7816e30b2fb610cf4fbaa
Doğukan Variş <varisdogukan@outlook.com>
2023-11-16 09:19:02 +0300

    remove a console log

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -11,8 +11,6 @@ export default async function Page() {
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
-
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
