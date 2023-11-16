dc4e065b21f378363bc164a7cb2676b50ec729ff
Doğukan Variş <varisdogukan@outlook.com>
2023-11-16 09:31:15 +0300

    Fixed error for vercel deploy

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -69,11 +69,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
-
-    console.log(userQuestionIds);
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
